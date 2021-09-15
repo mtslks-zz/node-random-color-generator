ca4022be19b8b39ffdd78525ac008ff7992e5a97
Mathias Lukas <mathias.lukas@me.com>
2021-09-15 09:48:10 +0200

    Redefine hue, luminosity variables

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const chalk = require('chalk');
 // Library to prompt the user for input
 const prompt = require('prompt-sync')();
 
-let hue = 'random';
-let luminosity = 'random';
-
-hue = process.argv[2];
-luminosity = process.argv[3];
+let hue = process.argv[2] ? process.argv[2] : 'random';
+let luminosity = process.argv[3] ? process.argv[3] : 'random';
 
 // Ask for user input at position 2
 if (process.argv[2] === 'ask') {
